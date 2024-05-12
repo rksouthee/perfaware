48ae7df44b572a0e05be160628fea667b34e136c
Robert Southee <rksouthee@gmail.com>
2024-05-12 12:29:58 +0100

    Opcode Patterns in 8086 Arithmetic

--- sim86/printer.cpp
+++ sim86/printer.cpp
@@ -27,6 +27,56 @@ namespace
 		return s_byte_registers[reg];
 	}
 
+	const char* const s_ops[8] =
+	{
+		"add", "or", "adc", "sbb", "and", "sub", "xor", "cmp"
+	};
+
+	const char* get_op(const std::size_t op)
+	{
+		return s_ops[op];
+	}
+
+	std::optional<sim86::PrintResult> print_mod_rm(const std::uint8_t* first, const std::uint8_t* last, const std::uint8_t mod, const std::uint8_t r_m, const std::uint8_t w, const bool add_prefix = false)
+	{
+		const std::string prefix = add_prefix ? (w ? " word " : " byte ") : "";
+		switch (mod)
+		{
+		case 0b00:
+			{
+				if (r_m == 0b110)
+				{
+					if (last - first < 2) return std::nullopt;
+					const std::uint16_t addr = first[0] | (first[1] << 8);
+					return sim86::PrintResult{ std::format("{}[{:#x}]", prefix, addr), first + 2 };
+				}
+				return sim86::PrintResult{ std::format("{}[{}]", prefix, s_ea_registers[r_m]), first };
+			}
+		case 0b01:
+			{
+				if (last - first < 1) return std::nullopt;
+				return sim86::PrintResult{
+					std::format("{}[{}{:+#x}]", prefix, s_ea_registers[r_m], static_cast<int>(static_cast<std::int8_t>(first[0]))),
+					first + 1
+				};
+			}
+		case 0b10:
+			{
+				if (last - first < 2) return std::nullopt;
+				const std::int16_t addr = first[0] | (first[1] << 8);
+				return sim86::PrintResult{
+					std::format("{}[{}{:+#x}]", prefix, s_ea_registers[r_m], addr),
+					first + 2
+				};
+			}
+		case 0b11:
+			{
+				return sim86::PrintResult{ get_register(r_m, w), first };
+			}
+		}
+		return std::nullopt;
+	}
+
 	struct Mod_reg_rm
 	{
 		std::string reg;
@@ -42,47 +92,12 @@ namespace
 		const std::uint8_t reg = (value >> 3) & 0x7;
 		const std::uint8_t mod = (value >> 6) & 0x3;
 
+		const std::optional<sim86::PrintResult> print_r_m = print_mod_rm(first, last, mod, r_m, w);
+		if (!print_r_m) return std::nullopt;
 		Mod_reg_rm result;
-		switch (mod)
-		{
-		case 0b00:
-			{
-				if (r_m == 0b110)
-				{
-					if (last - first < 2) return std::nullopt;
-					const std::uint16_t address = first[0] | (first[1] << 8);
-					result.r_m = std::format("[{:#x}]", address);
-					first += 2;
-				}
-				else
-				{
-					result.r_m = std::format("[{}]", s_ea_registers[r_m]);
-				}
-			}
-			break;
-		case 0b01:
-			{
-				if (last - first < 1) return std::nullopt;
-				result.r_m = std::format("[{}{:+#x}]", s_ea_registers[r_m], static_cast<int>(static_cast<std::int8_t>(first[0])));
-				++first;
-			}
-			break;
-		case 0b10:
-			{
-				if (last - first < 2) return std::nullopt;
-				const std::int16_t address = first[0] | (first[1] << 8);
-				result.r_m = std::format("[{}{:+#x}]", s_ea_registers[r_m], address);
-				first += 2;
-			}
-			break;
-		case 0b11:
-			{
-				result.r_m = get_register(r_m, w);
-			}
-			break;
-		}
+		result.r_m = print_r_m->code;
 		result.reg = get_register(reg, w);
-		result.end = first;
+		result.end = print_r_m->end;
 		return result;
 	}
 
@@ -97,115 +112,221 @@ namespace
 		return { oss.str(), last };
 	}
 
-	sim86::PrintResult mov_rm_reg(const std::uint8_t* first, const std::uint8_t* last, const bool w, const bool d)
+	sim86::PrintResult op_rm_reg(const char* op, const std::uint8_t* first, const std::uint8_t* last, const bool w, const bool d)
 	{
 		if (++first == last) return bytes(first - 1, last);
 		const std::optional<Mod_reg_rm> result = parse_mod_reg_rm(first, last, w);
 		if (!result) return bytes(first - 1, last);
 		std::ostringstream oss;
-		oss << "mov ";
+		oss << op << ' ';
 		if (d) oss << result->reg << ',' << result->r_m;
 		else oss << result->r_m << ',' << result->reg;
 		return { oss.str(), result->end };
 	}
 
-	sim86::PrintResult mov_reg_immed_8(const std::uint8_t* first, const std::uint8_t* last, const char* const reg)
+	sim86::PrintResult op_reg_immed_8(const char* op, const std::uint8_t* first, const std::uint8_t* last, const char* const reg)
 	{
 		if (last - first < 2) return bytes(first, last);
-		return { std::format("mov {},{:#x}", reg, static_cast<int>(first[1])), first + 2 };
+		return { std::format("{} {},{:#x}", op, reg, static_cast<int>(first[1])), first + 2 };
 	}
 
-	sim86::PrintResult mov_reg_immed_16(const std::uint8_t* first, const std::uint8_t* last, const char* const reg)
+	sim86::PrintResult op_reg_immed_16(const char* op, const std::uint8_t* first, const std::uint8_t* last, const char* const reg)
 	{
 		if (last - first < 3) return bytes(first, last);
 		const std::uint16_t immed = first[1] | (first[2] << 8);
-		return { std::format("mov {},{:#x}", reg, immed), first + 3 };
+		return { std::format("{} {},{:#x}", op, reg, immed), first + 3 };
 	}
 
-	PRINT_FN(mov_cl_immed)
-	{
-		return mov_reg_immed_8(first, last, "cl");
-	}
-
-	PRINT_FN(mov_ch_immed)
-	{
-		return mov_reg_immed_8(first, last, "ch");
-	}
-
-	PRINT_FN(mov_ax_immed)
-	{
-		return mov_reg_immed_16(first, last, "ax");
-	}
-
-	PRINT_FN(mov_cx_immed)
-	{
-		return mov_reg_immed_16(first, last, "cx");
-	}
-
-	PRINT_FN(mov_dx_immed)
-	{
-		return mov_reg_immed_16(first, last, "dx");
-	}
-
-	PRINT_FN(mov_mem_immed_8)
+	sim86::PrintResult op_mem_immed_8(const char* op, const std::uint8_t* first, const std::uint8_t* last)
 	{
 		if (++first == last) return bytes(first - 1, last);
 		const std::optional<Mod_reg_rm> result = parse_mod_reg_rm(first, last, 0);
 		if (!result || last - result->end < 1) return bytes(first - 1, last);
-		return { std::format("mov byte {},{:#x}", result->r_m, static_cast<int>(result->end[0])), result->end + 1 };
+		return { std::format("{} byte {},{:#x}", op, result->r_m, static_cast<int>(result->end[0])), result->end + 1 };
 	}
 
-	PRINT_FN(mov_mem_immed_16)
+	sim86::PrintResult op_mem_immed_16(const char* op, const std::uint8_t* first, const std::uint8_t* last)
 	{
 		if (++first == last) return bytes(first - 1, last);
 		const std::optional<Mod_reg_rm> result = parse_mod_reg_rm(first, last, 1);
 		if (!result || last - result->end < 2) return bytes(first - 1, last);
 		const std::uint16_t immed = result->end[0] | (result->end[1] << 8);
-		return { std::format("mov word {},{:#x}", result->r_m, immed), result->end + 2 };
+		return { std::format("{} word {},{:#x}", op, result->r_m, immed), result->end + 2 };
 	}
 
-	PRINT_FN(mov_rm_reg_8)
+	sim86::PrintResult op_ax_mem(const char* op, const std::uint8_t* first, const std::uint8_t* last)
 	{
-		return mov_rm_reg(first, last, 0, 0);
+		if (last - first < 3) return bytes(first, last);
+		const std::uint16_t addr = first[1] | (first[2] << 8);
+		return { std::format("{} ax,[{:#x}]", op, addr), first + 3 };
 	}
 
-	PRINT_FN(mov_rm_reg_16)
+	sim86::PrintResult op_mem_ax(const char* op, const std::uint8_t* first, const std::uint8_t* last)
 	{
-		return mov_rm_reg(first, last, 1, 0);
+		if (last - first < 3) return bytes(first, last);
+		const std::uint16_t addr = first[1] | (first[2] << 8);
+		return { std::format("{} [{:#x}],ax", op, addr), first + 3 };
 	}
 
-	PRINT_FN(mov_reg_rm_8)
+	PRINT_FN(mov_cl_immed)
 	{
-		return mov_rm_reg(first, last, 0, 1);
+		return op_reg_immed_8("mov", first, last, "cl");
 	}
 
-	PRINT_FN(mov_reg_rm_16)
+	PRINT_FN(mov_ch_immed)
 	{
-		return mov_rm_reg(first, last, 1, 1);
+		return op_reg_immed_8("mov", first, last, "ch");
+	}
+
+	PRINT_FN(mov_cx_immed)
+	{
+		return op_reg_immed_16("mov", first, last, "cx");
+	}
+
+	PRINT_FN(mov_dx_immed)
+	{
+		return op_reg_immed_16("mov", first, last, "dx");
+	}
+
+	PRINT_FN(mov_mem_immed_8)
+	{
+		return op_mem_immed_8("mov", first, last);
+	}
+
+	PRINT_FN(mov_mem_immed_16)
+	{
+		return op_mem_immed_16("mov", first, last);
 	}
 
 	PRINT_FN(mov_ax_mem)
 	{
-		if (last - first < 3) return bytes(first, last);
-		const std::uint16_t addr = first[1] | (first[2] << 8);
-		return { std::format("mov ax,[{:#x}]", addr), first + 3 };
+		return op_ax_mem("mov", first, last);
 	}
 
 	PRINT_FN(mov_mem_ax)
 	{
-		if (last - first < 3) return bytes(first, last);
-		const std::uint16_t addr = first[1] | (first[2] << 8);
-		return { std::format("mov [{:#x}],ax", addr), first + 3 };
+		return op_mem_ax("mov", first, last);
 	}
+
+	PRINT_FN(op_rm_8_immed_8)
+	{
+		if (++first == last) return bytes(first - 1, last);
+		const std::uint8_t value = *first++;
+		const std::uint8_t r_m = value & 0x7;
+		const std::uint8_t op = (value >> 3) & 0x7;
+		const std::uint8_t mod = (value >> 6) & 0x3;
+
+		const std::optional<sim86::PrintResult> print_rm = print_mod_rm(first, last, mod, r_m, 0);
+		if (!print_rm) return bytes(first - 2, last);
+		if (last - print_rm->end < 1) return bytes(first - 2, last); 
+		const std::uint8_t immed = print_rm->end[0];
+		return { std::format("{} {},byte {:#x}", get_op(op), print_rm->code, immed), print_rm->end + 1 };
+	}
+
+	PRINT_FN(op_rm_16_immed_8)
+	{
+		if (++first == last) return bytes(first - 1, last);
+		const std::uint8_t value = *first++;
+		const std::uint8_t r_m = value & 0x7;
+		const std::uint8_t op = (value >> 3) & 0x7;
+		const std::uint8_t mod = (value >> 6) & 0x3;
+
+		const std::optional<sim86::PrintResult> print_rm = print_mod_rm(first, last, mod, r_m, 1, 1);
+		if (!print_rm) return bytes(first - 2, last);
+		if (last - print_rm->end < 1) return bytes(first - 2, last); 
+		const std::uint8_t immed = print_rm->end[0];
+		return { std::format("{} {},byte {:+#x}", get_op(op), print_rm->code, immed), print_rm->end + 1 };
+	}
+
+	PRINT_FN(op_rm_16_immed_16)
+	{
+		if (++first == last) return bytes(first - 1, last);
+		const std::uint8_t value = *first++;
+		const std::uint8_t r_m = value & 0x7;
+		const std::uint8_t op = (value >> 3) & 0x7;
+		const std::uint8_t mod = (value >> 6) & 0x3;
+
+		const std::optional<sim86::PrintResult> print_rm = print_mod_rm(first, last, mod, r_m, 1);
+		if (!print_rm) return bytes(first - 2, last);
+		if (last - print_rm->end < 2) return bytes(first - 2, last); 
+		const std::uint16_t immed = print_rm->end[0] | (print_rm->end[1] << 8);
+		return { std::format("{} {},byte {:#x}", get_op(op), print_rm->code, immed), print_rm->end + 2 };
+	}
+
+	sim86::PrintResult print_jump(const char* jmp, const std::uint8_t* first, const std::uint8_t* last)
+	{
+		if (++first == last) return bytes(first - 1, last);
+		const std::int8_t offset = first[0];
+		/* return { std::format("{} short {:+#x}", jmp, offset), first + 1 }; */
+		return { std::format("{} ${:+#x}", jmp, offset + 2), first + 1 };
+	}
+
+#define JUMP(jmp)\
+	PRINT_FN(jmp##_short_label)\
+	{\
+		return print_jump(#jmp, first, last);\
+	}
+
+	JUMP(jo);
+	JUMP(jno);
+	JUMP(jc);
+	JUMP(jnc);
+	JUMP(jz);
+	JUMP(jnz);
+	JUMP(jna);
+	JUMP(ja);
+	JUMP(js);
+	JUMP(jns);
+	JUMP(jpe);
+	JUMP(jpo);
+	JUMP(jl);
+	JUMP(jnl);
+	JUMP(jng);
+	JUMP(jg);
+	JUMP(loope);
+	JUMP(loopne);
+	JUMP(loop);
+	JUMP(jcxz);
+
+#define BINARY_OPS(op)\
+	PRINT_FN(op##_rm_reg_8)\
+	{\
+		return op_rm_reg(#op, first, last, 0, 0);\
+	}\
+	PRINT_FN(op##_rm_reg_16)\
+	{\
+		return op_rm_reg(#op, first, last, 1, 0);\
+	}\
+	PRINT_FN(op##_reg_rm_8)\
+	{\
+		return op_rm_reg(#op, first, last, 0, 1);\
+	}\
+	PRINT_FN(op##_reg_rm_16)\
+	{\
+		return op_rm_reg(#op, first, last, 1, 1);\
+	}\
+	PRINT_FN(op##_al_immed)\
+	{\
+		return op_reg_immed_8(#op, first, last, "al");\
+	}\
+	PRINT_FN(op##_ax_immed)\
+	{\
+		return op_reg_immed_16(#op, first, last, "ax");\
+	}
+
+	BINARY_OPS(add);
+	BINARY_OPS(sub);
+	BINARY_OPS(cmp);
+	BINARY_OPS(mov);
 
 	const Print_fn s_printers[256] =
 	{
-		/* 0x00 */ bytes,
-		/* 0x01 */ bytes,
-		/* 0x02 */ bytes,
-		/* 0x03 */ bytes,
-		/* 0x04 */ bytes,
-		/* 0x05 */ bytes,
+		/* 0x00 */ add_rm_reg_8,
+		/* 0x01 */ add_rm_reg_16,
+		/* 0x02 */ add_reg_rm_8,
+		/* 0x03 */ add_reg_rm_16,
+		/* 0x04 */ add_al_immed,
+		/* 0x05 */ add_ax_immed,
 		/* 0x06 */ bytes,
 		/* 0x07 */ bytes,
 		/* 0x08 */ bytes,
@@ -240,12 +361,12 @@ namespace
 		/* 0x25 */ bytes,
 		/* 0x26 */ bytes,
 		/* 0x27 */ bytes,
-		/* 0x28 */ bytes,
-		/* 0x29 */ bytes,
-		/* 0x2a */ bytes,
-		/* 0x2b */ bytes,
-		/* 0x2c */ bytes,
-		/* 0x2d */ bytes,
+		/* 0x28 */ sub_rm_reg_8,
+		/* 0x29 */ sub_rm_reg_16,
+		/* 0x2a */ sub_reg_rm_8,
+		/* 0x2b */ sub_reg_rm_16,
+		/* 0x2c */ sub_al_immed,
+		/* 0x2d */ sub_ax_immed,
 		/* 0x2e */ bytes,
 		/* 0x2f */ bytes,
 		/* 0x30 */ bytes,
@@ -256,12 +377,12 @@ namespace
 		/* 0x35 */ bytes,
 		/* 0x36 */ bytes,
 		/* 0x37 */ bytes,
-		/* 0x38 */ bytes,
-		/* 0x39 */ bytes,
-		/* 0x3a */ bytes,
-		/* 0x3b */ bytes,
-		/* 0x3c */ bytes,
-		/* 0x3d */ bytes,
+		/* 0x38 */ cmp_rm_reg_8,
+		/* 0x39 */ cmp_rm_reg_16,
+		/* 0x3a */ cmp_reg_rm_8,
+		/* 0x3b */ cmp_reg_rm_16,
+		/* 0x3c */ cmp_al_immed,
+		/* 0x3d */ cmp_ax_immed,
 		/* 0x3e */ bytes,
 		/* 0x3f */ bytes,
 		/* 0x40 */ bytes,
@@ -312,26 +433,26 @@ namespace
 		/* 0x6d */ bytes,
 		/* 0x6e */ bytes,
 		/* 0x6f */ bytes,
-		/* 0x70 */ bytes,
-		/* 0x71 */ bytes,
-		/* 0x72 */ bytes,
-		/* 0x73 */ bytes,
-		/* 0x74 */ bytes,
-		/* 0x75 */ bytes,
-		/* 0x76 */ bytes,
-		/* 0x77 */ bytes,
-		/* 0x78 */ bytes,
-		/* 0x79 */ bytes,
-		/* 0x7a */ bytes,
-		/* 0x7b */ bytes,
-		/* 0x7c */ bytes,
-		/* 0x7d */ bytes,
-		/* 0x7e */ bytes,
-		/* 0x7f */ bytes,
-		/* 0x80 */ bytes,
-		/* 0x81 */ bytes,
+		/* 0x70 */ jo_short_label,
+		/* 0x71 */ jno_short_label,
+		/* 0x72 */ jc_short_label,
+		/* 0x73 */ jnc_short_label,
+		/* 0x74 */ jz_short_label,
+		/* 0x75 */ jnz_short_label,
+		/* 0x76 */ jna_short_label,
+		/* 0x77 */ ja_short_label,
+		/* 0x78 */ js_short_label,
+		/* 0x79 */ jns_short_label,
+		/* 0x7a */ jpe_short_label,
+		/* 0x7b */ jpo_short_label,
+		/* 0x7c */ jl_short_label,
+		/* 0x7d */ jnl_short_label,
+		/* 0x7e */ jng_short_label,
+		/* 0x7f */ jg_short_label,
+		/* 0x80 */ op_rm_8_immed_8,
+		/* 0x81 */ op_rm_16_immed_16,
 		/* 0x82 */ bytes,
-		/* 0x83 */ bytes,
+		/* 0x83 */ op_rm_16_immed_8,
 		/* 0x84 */ bytes,
 		/* 0x85 */ bytes,
 		/* 0x86 */ bytes,
@@ -376,7 +497,7 @@ namespace
 		/* 0xad */ bytes,
 		/* 0xae */ bytes,
 		/* 0xaf */ bytes,
-		/* 0xb0 */ bytes,
+		/* 0xb0 */ mov_al_immed,
 		/* 0xb1 */ mov_cl_immed,
 		/* 0xb2 */ bytes,
 		/* 0xb3 */ bytes,
@@ -384,7 +505,7 @@ namespace
 		/* 0xb5 */ mov_ch_immed,
 		/* 0xb6 */ bytes,
 		/* 0xb7 */ bytes,
-		/* 0xb8 */ bytes,
+		/* 0xb8 */ mov_ax_immed,
 		/* 0xb9 */ mov_cx_immed,
 		/* 0xba */ mov_dx_immed,
 		/* 0xbb */ bytes,
@@ -424,10 +545,10 @@ namespace
 		/* 0xdd */ bytes,
 		/* 0xde */ bytes,
 		/* 0xdf */ bytes,
-		/* 0xe0 */ bytes,
-		/* 0xe1 */ bytes,
-		/* 0xe2 */ bytes,
-		/* 0xe3 */ bytes,
+		/* 0xe0 */ loopne_short_label,
+		/* 0xe1 */ loope_short_label,
+		/* 0xe2 */ loop_short_label,
+		/* 0xe3 */ jcxz_short_label,
 		/* 0xe4 */ bytes,
 		/* 0xe5 */ bytes,
 		/* 0xe6 */ bytes,
