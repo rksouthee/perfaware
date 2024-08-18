cb3407a34e678215380c7087d5e8451f44f0bd56
Robert Southee <rksouthee@gmail.com>
2024-08-18 19:42:00 +0100

    Reformatting

--- part3/jump_alignment_main.cpp
+++ part3/jump_alignment_main.cpp
@@ -28,44 +28,44 @@ namespace
 
 	const Test_function s_test_functions[] =
 	{
-            { "nop_aligned_64", nop_aligned_64 },
-            { "nop_aligned_1", nop_aligned_1 },
-            { "nop_aligned_15", nop_aligned_15 },
-            { "nop_aligned_31", nop_aligned_31 },
-            { "nop_aligned_63", nop_aligned_63 }
+	{ "nop_aligned_64", nop_aligned_64 },
+	{ "nop_aligned_1", nop_aligned_1 },
+	{ "nop_aligned_15", nop_aligned_15 },
+	{ "nop_aligned_31", nop_aligned_31 },
+	{ "nop_aligned_63", nop_aligned_63 }
 	};
 }
 
 int main()
 {
-    const std::size_t buffer_size = 1 * 1024 * 1024 * 1024;
-    auto buffer = std::make_unique<std::uint8_t[]>(buffer_size);
+	const std::size_t buffer_size = 1 * 1024 * 1024 * 1024;
+	auto buffer = std::make_unique<std::uint8_t[]>(buffer_size);
 
-    if (!buffer)
-    {
-        std::cerr << "Failed to allocate buffer" << std::endl;
-        return EXIT_FAILURE;
-    }
+	if (!buffer)
+	{
+		std::cerr << "Failed to allocate buffer" << std::endl;
+		return EXIT_FAILURE;
+	}
 
-    Tester testers[std::size(s_test_functions)] = {};
-    while (true)
-    {
-        for (std::size_t i = 0; i < std::size(s_test_functions); ++i)
-        {
-            Tester& tester = testers[i];
-            Test_function function = s_test_functions[i];
+	Tester testers[std::size(s_test_functions)] = {};
+	while (true)
+	{
+		for (std::size_t i = 0; i < std::size(s_test_functions); ++i)
+		{
+			Tester& tester = testers[i];
+			Test_function function = s_test_functions[i];
 
-            std::cout << "\n--- " << function.name << " ---" << std::endl;
-            const std::uint64_t cpu_timer_frequency = perf::estimate_cpu_timer_freq();
-            new_test_wave(tester, buffer_size, cpu_timer_frequency);
+			std::cout << "\n--- " << function.name << " ---" << std::endl;
+			const std::uint64_t cpu_timer_frequency = perf::estimate_cpu_timer_freq();
+			new_test_wave(tester, buffer_size, cpu_timer_frequency);
 
-            while (is_testing(tester))
-            {
-                begin_time(tester);
-                function.function(buffer_size, buffer.get());
-                end_time(tester);
-                count_bytes(tester, buffer_size);
-            }
-        }
-    }
+			while (is_testing(tester))
+			{
+				begin_time(tester);
+				function.function(buffer_size, buffer.get());
+				end_time(tester);
+				count_bytes(tester, buffer_size);
+			}
+		}
+	}
 }
